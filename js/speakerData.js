2693b68ef8f6c8a4aaddeffc74f99c75dc50c712
amrendrakind <vision2010@gmail.com>
2022-03-16 02:27:20 +0530

    Add Speaker data JS file

---
+++ js/speakerData.js
@@ -0,0 +1,50 @@
+const data = {
+  speakers: [
+    {
+      name: 'Yochai Benkler',
+      tag_name: 'yohai benkler',
+      picture: 'images/speakers/speaker_01.png',
+      workplace: 'Professor at Harvard Law School',
+      description: `Focusing on a collaborative approach in a networked environment, he created the concept of
+                co-production based on sharing, such as open source software and Wikipedia.`,
+    },
+    {
+      name: 'Jeon Gil-nam',
+      tag_name: 'Kilnam Chon',
+      picture: 'images/speakers/speaker_02.png',
+      workplace: 'Emeritus Professor, Korea Advanced Institute of Science and Technology (KAIST)',
+      description: 'By developing Asia\'s first Internet protocol network SDN and leading Korea\'s first private line Internet connection in 1990, it ushered in the era of the Internet in earnest.',
+    },
+    {
+      name: 'Noh So-young',
+      tag_name: 'Sohyeong Noh',
+      picture: 'images/speakers/speaker_03.png',
+      workplace: 'Art Center Nabi Director, CC Korea Director',
+      description: 'As the author of &lt;Digital Art Art of Our Time&gt;, he opened \'Art Center Nabi\', Korea\'s first digital art institution in 2000, and is currently serving.',
+    },
+    {
+      name: 'Julia Leda',
+      tag_name: 'Julia Leda',
+      picture: 'images/speakers/speaker_04.png',
+      workplace: 'Head of the Young Pirates of Europe',
+      description: 'European integration and online youth participation in politics and democracy are major concerns, and a report has been published that will potentially affect the revision of the EU\'s copyright law in July.',
+    },
+    {
+      name: 'Layla Tretikov',
+      tag_name: 'Layla Tretikov',
+      picture: 'images/speakers/speaker_05.png',
+      workplace: 'Secretary General of the Wikimedia Foundation',
+      description: 'Layla Tretikov is the general secretary of the Wikimedia Foundation, a non-profit organization that runs Wikipedia.',
+    },
+
+    {
+      name: 'Ryan Merkley',
+      tag_name: 'Ryan Merkley',
+      picture: 'images/speakers/speaker_06.png',
+      workplace: 'Creative Commons CEO, Former Mozilla Foundation COO',
+      description: 'He has been active in open movements such as open government and open source.',
+    },
+  ],
+};
+
+export default data;
